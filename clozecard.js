c606cc088d50e1068a6f7851dca620ef80409a5f
Isaac Ivins <isaacivins@isaacs-air.attlocal.net>
2017-04-07 13:46:04 -0400

    minor_fixes

--- clozecard.js
+++ clozecard.js
@@ -2,7 +2,9 @@
 //===================
 //full = full text
 //cloze = answer
-//partial = full text - answer aka: question
+//partial = full text - answer (full - cloze) aka: question
+
+var ClozeCard = function(full, cloze) {
 	this.full = full;
 	this.cloze = cloze;
 	this.partial = this.full.replace(this.cloze, "...");
