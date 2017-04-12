8f8445374d100ce37908ae00ccd060e86933f4f3
Isaac Ivins <isaacivins@isaacs-air.attlocal.net>
2017-04-11 23:01:42 -0400

    written documentation and almost have game logic

--- creation.js
+++ creation.js
@@ -1,34 +1,49 @@
+//npm dependencies 
 var fs = require("fs");
 var os = require("os");
 var inquirer = require("inquirer");
+
+//module imports of constructors
 var BasicCard = require("./basiccard.js");
 var Clozecard = require("./clozecard.js");
+
+//arrays pre append to txt file
 var basicCarArray = [];
+var clozeCardArray = [];
+
+//arrays after imported from txt file
 var savedBasicCardArray = [];
 
+//function to see which type of flashcard is going to be created
 var whichType = function() {
+	//terminal responce for user input
 	inquirer.prompt([
 	{
 		name: "typeOfFlashcard",
 		type: "list",
 		message: "Which type of flashcards are you creating?",
 		choices: ["Basic Flashcards", "Clozecard Flashcards"]
-	}
+	} 
+		//once the user choses, run this part
 		]).then(function(answers) {
 			if (answers.typeOfFlashcard === "Basic Flashcards") {
 
 				console.log("======== Basic ========");
+				//calls basic flash card function
 				basicFLashcards();
 
 			} else {
 
 				console.log("======== Cloze ========");
+				//calls clozecard flash card function
 				clozeCardFlashcards();
 			}
 		});//ends then
 };//ends which card function
 
+//function for creating basic cards
 var basicFLashcards = function() {
+	//terminal responce for user input
 	inquirer.prompt([
 	{
 		name: "front",
@@ -36,8 +51,10 @@ var basicFLashcards = function() {
 	}, {
 		name: "back",
 		message: "What is the answer for the back?"
-	}, 
+	}
+		//once the user choses, run this part
 		]).then(function(answers) {
+			//constructs BasicCard as a flashcard with (front and back)
 			var newCard = new BasicCard(
 				answers.front,
 				answers.back);
@@ -45,7 +62,7 @@ var basicFLashcards = function() {
 				console.log("======== Card Created =======")
 				basicCarArray.push(newCard);
 				console.log(basicCarArray);
-
+				//after cards created, asks user if they want to create another
 				inquirer.prompt([
 				{
 					name: "createAgain",
@@ -56,11 +73,12 @@ var basicFLashcards = function() {
 					]).then(function(answers) {
 
 						if (answers.createAgain === "yes") {
+							//re runs the function if user wants to create more than one card
 							basicFLashcards();
 						} else {
 							console.log("====== Basic Flashcards ========")
 							console.log(basicCarArray)
-
+							//takes flashcards and moves them to a text file
 							basicCardPush();
 
 						}
@@ -68,14 +86,16 @@ var basicFLashcards = function() {
 			})//end then 1
 };//end Basic function
 
+//takes flashcards and moves them to a text file
 var basicCardPush = function() {
-
+	// creates the string to append to the txt file cleaner
 	var logString = '';
 
-
+	//loops throught the basic card array of basic card constructors
 	for (var i in basicCarArray) {
 
 		if (i < basicCarArray.length){
+			//this takes the front and back and puts it on one line, os.EOL creates a linebreak
 			logString += basicCarArray[i].front + ',' + basicCarArray[i].back + os.EOL;
 		}
 		else {
@@ -84,22 +104,21 @@ var basicCardPush = function() {
 
 		
 	}
-		// basicCardString = JSON.stringify(basicCarArray);
-		// console.log("first: " + basicCardString);
-
+	// the npms fs call to append the logstring to the txt file
 	fs.appendFile("basicCard.txt", logString, function(err) {
 		if (err) {
 			console.log(err);
 		} else {
 			console.log("=========Content Added!=========");
+			//if there's no error this will essentially send the user to the beginging of the app
 			playOrCreate();
 		}
 	})
 };//end basicCardPush()
 
-var clozeCardArray = [];
-
+//function for creating cloze cards
 var clozeCardFlashcards = function() {
+	//terminal responce for user input
 	inquirer.prompt([
 	{
 		name: "full",
@@ -108,9 +127,11 @@ var clozeCardFlashcards = function() {
 		name: "cloze",
 		message: "Which part would you like to be removed?"
 	}
+		// once you gather input run this
 		]).then(function(answers) {
-
+			//this creates partial from the rull and cloze answers
 			var partial = answers.full.replace(answers.cloze, "...");
+			//uses the Clozecard constructor to create Clozecard Flashcards
 			var newCard = new Clozecard(
 				answers.full,
 				answers.cloze,
@@ -119,8 +140,7 @@ var clozeCardFlashcards = function() {
 				console.log("======== Card Created =======")
 				clozeCardArray.push(newCard);
 				console.log(clozeCardArray);
-				//push the new card into a text file here
-
+				// after construction this asks if the want to make more than one
 				inquirer.prompt([
 				{
 					name: "createAgain",
@@ -131,20 +151,22 @@ var clozeCardFlashcards = function() {
 					]).then(function(answers) {
 
 						if (answers.createAgain === "yes") {
+							//if the user wants more than one, re-run this function
 							clozeCardFlashcards();
 						} else {
 							console.log("======== Clozecard Flashcards ========")
 							console.log(clozeCardArray)
 							console.log("======================================")
-
+							//this is subject to change for once i figure out how to do basic
 							clozeCardArray = JSON.stringify(clozeCardArray);
+							//pushed the flashcard constructors to a txt file
 							clozeCardPush();
 						}
 					})//end then 2
 		})//end then 1
 }//end Cloze Function
 
-
+//ALMOST ALL OF THIS WILL CHANGE, LIKE 85% SO KEEP THAT IN MIND
 var clozeCardPush = function() {
 	fs.appendFile("clozeCard.txt", "&" + clozeCardArray, function(err) {
 		if (err) {
@@ -155,19 +177,24 @@ var clozeCardPush = function() {
 	})
 };//end clozeCardPush
 
+//this is the money function, first function run, asks to play or create flashcards
 var playOrCreate = function() {
+	//asks play game or create flashcards
 	inquirer.prompt([
 	{
 		name: "start",
 		type: "list",
 		message: "Would you like to Play Game or Create Flashcards?",
 		choices: ["Play Game", "Create Flashcards"]
-	}
+	}	
+		//after we know what to do run this
 		]).then(function(answers) {
 
 			if (answers.start === "Create Flashcards") {
+				//if they want to create flashcards, this function will ask what type
 				whichType();
 			} else if (answers.start === "Play Game") {
+				//underconsturction at the moment, but also needs to specify what cards for game
 				playGame();
 			} else {
 				console.log("WTF DID YOU DO!?!?!?")
@@ -175,8 +202,9 @@ var playOrCreate = function() {
 		})//end then 1
 }// end play or create()
 
+//fucntion to text users knowledge of flashcards
 var playGame = function() {
-
+	//first we ask if the user is playing with Basic or Clozecard Flashcards
 	inquirer.prompt([
 	{
 		name: "BasicOrCloze",
@@ -188,9 +216,11 @@ var playGame = function() {
 
 
 			if (answers.BasicOrCloze === "Basic Flashcards") {
+				//if they answer basic flashcards, run the game logic with basic
 				basicGame();
 			} else if (answers.BasicOrCloze === "Clozecard Flashcards") {
 				console.log("working CLOZECARD flashcards");
+				//this is under construction for the near future
 				//clozeGame();
 			} else {
 				console.log("WTF DID YOU DO!?!?!?")
@@ -199,9 +229,10 @@ var playGame = function() {
 
 }//end playGame()
 
+//this is the function to play a game with created basic cards
 var basicGame = function() {
 
-
+	//fs npm to read the basic txt file to gather flashcard data
 	fs.readFile("basicCard.txt", "utf8", function(error, data) {
 
 		if (error) {
@@ -209,13 +240,23 @@ var basicGame = function() {
 
 		} else if (!error) {
 
+			//splits the txt file string at linebreaks (os.EOL)
 			var savedBasicCardArray = data.split(os.EOL);
 
 			console.log("==================")
 
+			console.log("array length ", savedBasicCardArray.length);
+
+			console.log("==================")
+
 				for (var i = 0; i < savedBasicCardArray.length - 1; i++) {
+					console.log("array length ",savedBasicCardArray.length)
 					console.log(savedBasicCardArray[i].split(','));
-					console.log(savedBasicCardArray[i][0])
+					// savedBasicCardArray has 5 lines (4 + extra line)
+					// need to loop through the 4, create objects (q & a's)
+					// use inquire to loop through objects and ask q's
+					// check q's with a's and score (correct, incorrect)
+					// track wins and losses
 				}
 
 			}
@@ -224,15 +265,8 @@ var basicGame = function() {
 };
 
 var clozeGame = function() {
-
-		//looop through cards and have (partial be question, cloze be answer)
-		//randomize card order and ask sequentially untill no cards are left
+		//just fuckin mimic the basic card one once you figure it out
 };
 
-
-
-
+//function to "put on for my city"-Jeeze? maybe lil wayne? idk it starts the app
 playOrCreate();
-
-
-
